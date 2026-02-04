b4b06df895ba7ac3147df84c82cc64cff54767fb
pOcOq38 <cylee0322@gmail.com>
2026-02-04 14:46:37 -0800

    feat: add list of mock data and result list

--- my-first-angular-app/src/app/housing-location/housing-location.ts
+++ my-first-angular-app/src/app/housing-location/housing-location.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, input } from '@angular/core';
+import { HousingLocationInfo } from '../housinglocation';
 @Component({
   selector: 'app-housing-location',
   imports: [],
@@ -7,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './housing-location.css',
 })
 export class HousingLocation {
+  //using signal to pass the housing location info to the component
+  //to output formatted text like {{housingLocation().name}} in the template
+  housingLocation = input.required<HousingLocationInfo>();
 
 }
