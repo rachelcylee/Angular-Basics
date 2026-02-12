6eab344c3a865814849360e4a3212123d83148fc
pOcOq38 <cylee0322@gmail.com>
2026-02-11 18:31:33 -0800

    feat: add search filter

--- my-first-angular-app/src/app/home/home.ts
+++ my-first-angular-app/src/app/home/home.ts
@@ -11,7 +11,23 @@ import { HousingService } from '../housing.service';
 export class Home {
     housingLocationList: HousingLocationInfo[] = [];
     housingService: HousingService = inject(HousingService);
+    filteredLocationList: HousingLocationInfo[] = [];
+
     constructor() { //a method runs automatically when a component is created.
         this.housingLocationList = this.housingService.getAllHousingLocations();
+        this.filteredLocationList = this.housingLocationList;
+    }
+
+    filterResults(text: string) {
+
+        if (!text) {
+            this.filteredLocationList = this.housingLocationList;
+
+            return;
+        }
+
+        this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+            housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+        );
     }
 }
